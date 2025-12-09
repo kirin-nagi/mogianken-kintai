ba6c726798cdc65533139d07303cfb53a62fd50f
kirin <fnagixjinf38@gmail.com>
2025-12-09 22:30:22 +0900

    20251209

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COACHTECH</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+</head>
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <div class="header-utilities">
+                <a class="header__img" href="">
+                    <img src='/storage/image/logo.svg' width="250">
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
