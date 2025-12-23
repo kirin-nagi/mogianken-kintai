60ae25f8cf8bb80a0e8c07c4588cd7dd9958b657
kirin <fnagixjinf38@gmail.com>
2025-12-24 00:40:12 +0900

    20251223

--- src/app/Http/Controllers/AttendanceController.php
+++ src/app/Http/Controllers/AttendanceController.php
@@ -4,22 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Attendance;
-use App\Models\Rest;
 use Carbon\Carbon;
 
 
 class AttendanceController extends Controller
 {
+
+    // 勤怠打刻画面
     public function showattendance()
     {
-        return view('attendance.attendance');
+        $attendance = Attendance::todayForUser();
+
+        return view('attendance.attendance', compact('attendance'));
     }
 
-    public function attendance()
+    public function start()
     {
-        $attendance = Attendance::where('user_id', auth()->id())->where('work_date', today())->first();
+        if(Attendance::todayForUser()){
+            return redirect()->route('attendance.attendance');
+        }
 
+        Attendance::create([
+            'user_id' => auth()->id(),
+            'start_time' => now(),
+        ]);
 
+        return redirect()->route('attendance.attendance');
     }
 
+    
 }
--- src/app/Http/Controllers/RestController.php
+++ src/app/Http/Controllers/RestController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class StampController extends Controller
+class RestController extends Controller
 {
     //
 }
