5e0fd8ed03666b6e8a3f7c510d082047402a6b94
kirin <fnagixjinf38@gmail.com>
2026-01-12 02:11:10 +0900

    20260111

--- src/resources/views/attendance/list.blade.php
+++ src/resources/views/attendance/list.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/list.css') }}">
+<link rel="stylesheet" href="/css/list.css">
 @endsection
 
 @section('content')
