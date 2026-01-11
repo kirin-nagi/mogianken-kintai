56a9f8cf00c08390d9420be6332dc71344985b63
kirin <fnagixjinf38@gmail.com>
2026-01-11 18:35:25 +0900

    20260111

--- src/resources/views/attendance/list.blade.php
+++ src/resources/views/attendance/list.blade.php
@@ -1,4 +1,3 @@
-<!-- 勤怠一覧 -->
 @extends('layouts.app')
 
 @section('css')
@@ -22,60 +21,64 @@
         </a>
     </div>
     <table>
-        <td colspan="6" class="empty-row">
-            <div class="empty-columns">
-                <span>日付</span>
-                <span>出勤</span>
-                <span>退勤</span>
-                <span>休憩</span>
-                <span>合計</span>
-                <span>詳細</span>
-            </div>
-        </td>
+        <tr>
+            <td colspan="6" class="empty-row">
+                <div class="empty-columns">
+                    <span>日付</span>
+                    <span>出勤</span>
+                    <span>退勤</span>
+                    <span>休憩</span>
+                    <span>合計</span>
+                    <span>詳細</span>
+                </div>
+            </td>
+        </tr>
 
         @foreach($dates as $date)
         @php
         $attendance = $attendances[$date->format('Y-m-d')] ?? null;
         @endphp
-        <td colspan="6" class="empty-row">
-            <div class="empty-columns">
-                <span>
+        <tr>
+            <td colspan="6" class="empty-row">
+                <div class="empty-columns">
+                    <span>
                     <!-- 日付 -->
-                {{ $date->format('m/d') }}
-                ({{ [ '日','月','火','水','木','金','土',][$date->dayOfWeek]}})
-                </span>
-                @if($attendance)
-                <span>
+                    {{ $date->format('m/d') }}
+                    ({{ [ '日','月','火','水','木','金','土',][$date->dayOfWeek]}})
+                    </span>
+                    @if($attendance)
+                    <span>
                     <!-- 出勤 -->
-                {{ $attendance->start_work->format('H:i') }}
-                </span>
-                <span>
+                    {{ $attendance->start_work->format('H:i') }}
+                    </span>
+                    <span>
                     <!-- 退勤 -->
-                {{ $attendance->end_work?->format('H:i') ?? '-' }}
-                </span>
-                <span>
+                    {{ $attendance->end_work?->format('H:i') ?? '-' }}
+                    </span>
+                    <span>
                     <!-- 休憩合計 -->
-                {{ $attendance->rests->sum('rest_time') ? $attendance->total_rest_formatted : '0:00' }}
-                </span>
-                <span>
+                    {{ $attendance->rests->sum('rest_time') ? $attendance->total_rest_formatted : '0:00' }}
+                    </span>
+                    <span>
                     <!-- 勤務合計 -->
-                {{ $attendance->total_work_formatted }}
-                </span>
-                <span>
-                    <a href="{{ route('attendance.detail', $attendance->id) }}" class="detail-item">詳細</a>
-                </span>
-                @else
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span>
-                    <a href="{{ route('attendance.detail', $date->format('Y-m-d')) }}" class="detail-item">詳細</a>
-                </span>
-                @endif
-            </div>
-        </td>
+                    {{ $attendance->total_work_formatted }}
+                    </span>
+                    <span>
+                        <a href="{{ route('attendance.detail', $attendance->id) }}" class="detail-item">詳細</a>
+                    </span>
+                    @else
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span>
+                        <a href="{{ route('attendance.detail', $date->format('Y-m-d')) }}" class="detail-item">詳細</a>
+                    </span>
+                    @endif
+                </div>
+            </td>
+        </tr>
         @endforeach
     </table>
 </div>
