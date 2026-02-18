45a81bd486a807bbf3c3fee5637cefc243279d43
kirin <fnagixjinf38@gmail.com>
2026-02-18 15:05:54 +0900

    20260218

--- src/resources/views/attendance/detail.blade.php
+++ src/resources/views/attendance/detail.blade.php
@@ -6,6 +6,62 @@
 
 
 @section('content')
+@if($viewState === 'pending')
+<div class="stamp-correction__content">
+    <div class="stamp-correction__heading">
+        <p class="stamp-correction__heading-item">勤怠詳細</p>
+    </div>
+    <div class="stamp-correction__table">
+        <table class="stamp-correction__inner">
+            <tr class="stamp-correction__item">
+                <th class="heading-item">名前</th>
+                <td class="text-item">
+                    <p class="approval-name">{{ $approval->user->name }}</p>
+                </td>
+            </tr>
+            <tr class="stamp-correction__item">
+                <th class="heading-item">日付</th>
+                <td class="year-date">
+                    <p class="top-item">{{ $approval->detail->work_date->format('Y') }}年</p>
+                    <p class="bottom-item">{{ $approval->detail->work_date->format('n月j日') }}</p>
+                </td>
+            </tr>
+            <tr class="stamp-correction__item">
+                <th class="heading-item">出勤・退勤</th>
+                <td class="date-time">
+                    <p class="top-item">{{ $approval->detail->start_work->format('H:i') }}</p>
+                    <span class="time-separator">～</span>
+                    <p class="bottom-item">{{ $approval->detail->end_work->format('H:i') }}</p>
+                </td>
+            </tr>
+            <tr class="stamp-correction__item">
+                <th class="heading-item">休憩</th>
+                <td class="date-time">
+                    <p class="top-item">{{ $approval->detail->rest_start->format('H:i') }}</p>
+                    <span class="time-separator">～</span>
+                    <p class="bottom-item">{{ $approval->detail->rest_end->format('H:i') }}</p>
+                </td>
+            </tr>
+            <tr class="stamp-correction__item">
+                <th class="heading-item">休憩２</th>
+                <td class="date-time">
+                    <p class="top-item">{{ $approval->detail->rest_start2?->format('H:i') }}</p>
+                    @if($approval->detail->rest_start2 && $approval->detail->rest_end2)
+                    <span class="time-separator">～</span>
+                    @endif
+                    <p class="bottom-item">{{ $approval->detail->rest_end2?->format('H:i') }}</p>
+                </td>
+            </tr>
+            <tr class="stamp-correction__item">
+                <th class="heading-item">備考</th>
+                <td class="text-item">
+                    <p class="detail-reason">{{ $approval->detail->reason }}</p>
+                </td>
+            </tr>
+        </table>
+    </div>
+<div>
+    @else
 <div class="detail-form__content">
     <div class="detail__heading">
         <p class="detail__heading-item">勤怠詳細</p>
@@ -97,6 +153,7 @@
                 </div>
             </div>
         </div>
+        @endif
         <div class="form__button">
             @if($viewState === 'request')
             <button class="form__button-submit" type="submit">修正</button>
